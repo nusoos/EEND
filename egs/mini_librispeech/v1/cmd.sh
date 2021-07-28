d0da21712f1cd086c48a77c2d592886886544726
Sebastian Klinger <klingerse62324@th-nuernberg.de>
2021-07-28 20:01:23 +0200

    Changed job scheduling to local machine (run.pl).

--- egs/mini_librispeech/v1/cmd.sh
+++ egs/mini_librispeech/v1/cmd.sh
@@ -2,14 +2,14 @@
 # For more information about cmd.sh see http://kaldi-asr.org/doc/queue.html.
 #
 # If you use your local machine, use "run.pl".
-# export train_cmd="run.pl"
-# export infer_cmd="run.pl"
-# export simu_cmd="run.pl"
+export train_cmd="run.pl"
+export infer_cmd="run.pl"
+export simu_cmd="run.pl"
 
 # If you use Grid Engine, use "queue.pl"
-export train_cmd="queue.pl --mem 16G -l 'hostname=c*'"
-export infer_cmd="queue.pl --mem 16G -l 'hostname=c*'"
-export simu_cmd="queue.pl"
+# export train_cmd="queue.pl --mem 16G -l 'hostname=c*'"
+# export infer_cmd="queue.pl --mem 16G -l 'hostname=c*'"
+# export simu_cmd="queue.pl"
 
 # If you use SLURM, use "slurm.pl".
 # export train_cmd="slurm.pl"
